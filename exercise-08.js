42f6fe4c021af4e18959e4ae5b04ad15f563c408
Rudy Harun Arrasyid <57350097+RudyHarunArrasyid@users.noreply.github.com>
2020-04-02 10:08:43 +0700

    week3

--- exercise-08.js
+++ exercise-08.js
@@ -1,18 +1,13 @@
 function pasanganTerbesar(num) {
-    var arrAngka = [];
-    var angka = 0;
-    var strNum = num.toString();
-    // membuat list isi pasangan angka
-    for (var i = 0; i < strNum.length - 1; i++) {
-      angka = Number(strNum[i] + strNum[i + 1]);
-      arrAngka.push(angka);
-    }
-
-    // mencari nilai terbesar
     var maxAngka = 0;
-    for (var i = 0; i < arrAngka.length; i++) {
-      if (arrAngka[i] > maxAngka) {
-        maxAngka = arrAngka[i];
+    var strNum = num.toString();
+
+    // membuat pasangan nilai
+    for (var i = 0; i < strNum.length - 1; i++) {
+      var angka = Number(strNum[i] + strNum[i + 1]);
+      // mencari nilai terbesar
+      if (angka > maxAngka) {
+        maxAngka = angka;
       }
     }
 
